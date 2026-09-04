a70ba5aeaa65eebc5f0eea0c565acc36e6760db2
Luis fereira <fereiraluis18051997@gmail.com>
2026-09-03 22:17:56 -0300

    fix: correcion del jwt

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,7 +20,7 @@ import { DashboardModule } from './modules/dashboard/dashboard.module';
     ConfigModule.forRoot({
       isGlobal: true,
       load: [appConfig, jwtConfig],
-      ignoreEnvFile: true
+      validationSchema: envValidationSchema,
     }),
     PrismaModule,
     AuthModule,
